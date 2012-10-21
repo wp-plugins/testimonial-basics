b92b746ea405ba1e8104346cbc4552b8d095dd1d
kevinhaig <kevinhaig@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-21 14:46:05 +0000

    Tagging 2.0.1
    git-svn-id: https://plugins.svn.wordpress.org/testimonial-basics@615252 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/2.0.1/includes/katb_captcha.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * This file contains the that is called when users select to use the captcha.
+ *
+ *
+ * @package		Testimonial Basics WordPress Plugin
+ * @copyright	Copyright (c) 2012, Kevin Archibald
+ * @license		http://www.gnu.org/licenses/quick-guide-gplv3.html  GNU Public License
+ * @author		Kevin Archibald <www.kevinsspace.ca/contact/>
+ */
+?>
+<?php
+ //Start session if not already started
+if(!isset($_SESSION)) session_start();
+
+  // Set some important CAPTCHA constants
+  define('CAPTCHA_NUMCHARS', 6);  // number of characters in pass-phrase
+  define('CAPTCHA_WIDTH', 110);   // width of image
+  define('CAPTCHA_HEIGHT', 22);   // height of image
+
+  // Generate the random pass-phrase
+  $pass_phrase = "";
+  for ($i = 0; $i < CAPTCHA_NUMCHARS; $i++) {    $pass_phrase .= chr(rand(97, 122));  }
+  
+    // Store the encrypted pass-phrase in a session variable
+  $_SESSION['pass_phrase'] = SHA1($pass_phrase);
+
+  // Create the image  
+  $img = imagecreatetruecolor(CAPTCHA_WIDTH, CAPTCHA_HEIGHT);  
+  // Set a white background with black text and gray graphics  
+  $bg_color = imagecolorallocate($img, 255, 255, 255);		// white  
+  $text_color = imagecolorallocate($img, 0, 0, 0);   		// black  
+  $graphic_color = imagecolorallocate($img, 64, 64, 64);   	// dark gray
+
+  // Fill the background
+  imagefilledrectangle($img, 0, 0, CAPTCHA_WIDTH, CAPTCHA_HEIGHT, $bg_color);
+  // Draw some random lines
+  for ($i = 0; $i < 5; $i++) {
+    imageline($img, 0, rand() % CAPTCHA_HEIGHT, CAPTCHA_WIDTH, rand() % CAPTCHA_HEIGHT, $graphic_color);
+  }
+
+  // Sprinkle in some random dots
+  for ($i = 0; $i < 50; $i++) {
+    imagesetpixel($img, rand() % CAPTCHA_WIDTH, rand() % CAPTCHA_HEIGHT, $graphic_color);
+  }
+  // Draw the pass-phrase string
+  imagettftext($img, 18, 0, 5, CAPTCHA_HEIGHT - 5, $text_color, 'Vera.ttf', $pass_phrase);
+
+  // Output the image as a PNG using a header  
+  header("Content-type: image/png");  
+  imagepng($img);
+
+  // Clean up
+  imagedestroy($img);
+?>
